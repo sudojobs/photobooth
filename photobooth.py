fb0a82ab993320b33786a8ea23b7be1a093cbda0
sudojobs <shobjobs@gmail.com>
2020-03-07 21:33:25 +0530

    update

--- photobooth.py
+++ photobooth.py
@@ -2,59 +2,37 @@ import os
 import sys
 from time import sleep
 from omxplayer import OMXPlayer
-from gpiozero import MotionSensor
 from signal import pause
-from picamera import PiCamera
 
-pir = MotionSensor(23)
 
-camera = PiCamera()
-vid1 = OMXPlayer('/home/pi/Videos/Vid_Wait_Loop1.mp4',args=['--win', '100 100 640 480','--loop'])
-vid2 = OMXPlayer('/home/pi/Videos/Vid_Name_Hammerstein.mp4',args=['--win', '100 100 640 480'])
+
+takevideo ='./videos/takphoto.mp4'
+
+cam0='/dev/video0'
+cam1='/dev/video1'
 
 def main():
-    initiate()
     while True:
         print("playing vid1")
-        vid1.play()
-        if pir.motion_detected:
-            motion_detected()
-        else:
-            no_motion()
-    except KeyboardInterrupt:
-        print("terminated by user")
-        vid1.quit()
-        vid2.quit()
-        camera.close()
-            try:
-                sys.exit(0)
-            except SystemExit:
-                os.exit(0)
-    finally:
-           vid1.quit()
-           vid2.quit()
-         camera.close()
-     print("bye bye")
+        #Call Subprocess Video via Myplayer
+        if (rfid_detected()==1):
+            rfid_process()
 
-def initiate():
-    print("CENTRAL AI Startup - Running initial setup")
-    sleep(1)
-    print("Starting Central AI visual front end")
-    sleep(1)
-    print("Motion detection activated")
-    sleep(1)
-    print("Security system activated")
+def rfid_process():
+    #play the webcam feed
+    #count down on screen 3, 2, 1
+    #click pic from cam0
+    #click pic from cam1
+    #loop Video MP4 "Processing Video"
+    #overlay_water_mark via ImageMagic
+    #create wigglegram mp4 video
+    #play the video for 30 seconds
+    #prints_outthe_photo
+    #go back to "take a photo"
+    #send output for approval
+    #send mp4 to tv in bar
 
-def no_motion():
-    print("All quiet")
-    vid1.play()
-
-def motion_detected():
-    print("Intruder Detected")
-    sleep(2)
-    print("Pausing loop")
-    vid1.pause()
-    print("Playing Vid_Name_Hammerstein")
-    vid2.play()
-    sleep(10)
-    vid2.pause()
+def rfid_detected():
+    #take the input from rfid
+    #if input is valid then
+    #return 1
