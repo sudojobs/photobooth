8b9767f089e3808d0a48d2b3392d1c17ca5a510c
sudojobs <shobjobs@gmail.com>
2020-03-12 20:27:17 +0530

    update

--- dummy.py
+++ dummy.py
@@ -1,7 +1,20 @@
 from glob import glob
 from os import stat
 
+def reverse(lst):
+    return [ele for ele in reversed(lst)]
+
 files = glob("./videos/*.mp4")
 sorted_list = sorted(files, key=lambda x: stat(x).st_mtime)
 
-truncated_list = sorted_list[-10:]
+truncated_list=reverse(sorted_list)
+#truncated_list = sorted_list
+
+print(truncated_list)
+hundred_list=truncated_list[-100:]
+
+
+with open('your_file.txt', 'w') as f:
+        for item in hundred_list:
+                    f.write("%s\n" % item)
+
--- tv.py
+++ tv.py
@@ -7,11 +7,12 @@ import time
 import datetime
 import subprocess
 
+from os import stat
 from time import sleep
 from os import listdir
 from subprocess import check_output
 from time import gmtime,strftime
-#from omxplayer import OMXPlayer
+from random import shuffle
 from omxplayer.player import OMXPlayer
 #-------------------------------
 #initialization
@@ -26,63 +27,36 @@ def reverse(lst):
 video_files = [ f for f in listdir('./videos') if f[-4:] == '.mp4' ]
 video_files_tmp = [ f for f in listdir('./videos') if f[-4:] == '.mp4' ]
 vid_files=glob.glob('./videos/*')
-latest_file = max(vid_files, key=os.path.getctime)
+
+if(len(vid_files)>0): 
+   latest_file = max(vid_files, key=os.path.getctime)
 
 if not (len(video_files) > 0):
-    print ("No mp4 files found!")
-
-#-------------------------------
-#function for video skip
-#-------------------------------
-
-def videoskip(channel):
-    subprocess.call(['killall', 'omxplayer.bin'])
-
-#-------------------------------
-#function for concatenate
-#-------------------------------
-
-def concatenate():
-	stringa = "ffmpeg -i \"concat:"
-	elenco_video = glob.glob("*.mp4")
-	elenco_file_temp = []
-	for f in elenco_video:
-		file = "temp" + str(elenco_video.index(f) + 1) + ".ts"
-		os.system("ffmpeg -i " + f + " -c copy -bsf:v h264_mp4toannexb -f mpegts " + file)
-		elenco_file_temp.append(file)
-	print(elenco_file_temp)
-	for f in elenco_file_temp:
-		stringa += f
-		if elenco_file_temp.index(f) != len(elenco_file_temp)-1:
-			stringa += "|"
-		else:
-			stringa += "\" -c copy  -bsf:a aac_adtstoasc output.mp4"
-	print(stringa)
-	os.system(stringa)    
+   print ("No mp4 files found!")
 
 #-------------------------------
 # Main Loop
 #-------------------------------
 while True:
        #check the video files if new file is there update the video_files(array list)
-       vid_files=glob.glob('./videos/*')
+       vid_files=glob.glob('./videos/*.mp4')
        #by default the process has been started and video playing
-       if(latest_file!= max(vid_files, key=os.path.getctime)):
-          video_files_tmp = [ f for f in listdir('./videos') if f[-4:] == '.mp4' ]
-          video_files=reverse(video_files_tmp)
-          latest_file=max(vid_files,key=os.path.getctime)
-          stateindex=0
- 
- 
-       subprocess.call(['omxplayer','--no-osd','--no-keys','-p','-o','hdmi', video_files[stateindex]])
- 
-       #after 1st mp4 file played we have increase the index size by 1 
-       stateindex += 1
-     
-       #loop if all the files are played if number is less than  99
-       if stateindex >= len(video_files):
-          stateindex = 0
-       elif stateindex ==99:
-          stateindex = 0
-       
-       sleep(0); 
+       if(len(vid_files)>0): 
+          if(latest_file!= max(vid_files, key=os.path.getctime)):
+             latest_file=max(vid_files,key=os.path.getctime)
+             sorted_list=sorted(vid_files, key=lambda x: stat(x).st_mtime)
+             hundred_list=reverse(sorted_list[-100:]) 
+             print(hundred_list)
+             extra_list=sorted_list[0:-99] 
+             if (os.path.exists('playlist.txt')):
+                 os.remove('playlist.txt')  
+             with open('playlist.txt', 'w') as f: # Create the playlist file for ffpmpeg capt
+                     for item in hundred_list:
+                         f.write("%s\n" % item)
+             for i in range (len(extra_list)):  # Rremove the extra files
+                     os.remove(extra_list[i])
+             subprocess.call(['ffmpeg','-f','concat','-i','playlist.txt','-c','copy','output.mp4'])
+             subprocess.call(['killall','omxplayer.bin']) 
+
+       subprocess.call(['omxplayer','--no-osd','--no-keys','--win', '1,1,500,400','-p','-o','hdmi', 'output.mp4'])
+       #sleep(0); 
